e5fd2ccac716129b22c5e0495b2a4d1bddf18129
Mike Dilley <mike.dilley@gmail.com>
2018-12-18 22:04:29 +0000

    Automatically backed up by Learn

--- db/migrate/20180314210852_create_users.rb
+++ db/migrate/20180314210852_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def up
-    create_table :users do |t|
+    create_table :user do |t|
       t.string :username
       t.string :password_digest
     end
